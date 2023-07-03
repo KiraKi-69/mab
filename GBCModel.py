36da629f91f8d76430dbe6aaf3d4dfee395f469d
Alex Gorokhov <agorokhov@neoflex.ru>
2023-07-03 14:13:35 +0300

    feat: import joblib

--- GBCModel.py
+++ GBCModel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import OrdinalEncoder
-
+import joblib
 class GBCModel(object):  
     
     def __init__(self):
