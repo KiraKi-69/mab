1be90be2222a5a2ca15ca2a4aa0c78bd97bd3d24
KiraKi <kirakotkova@gmail.com>
2023-07-04 17:53:03 +0300

    try

--- GBCModel.py
+++ GBCModel.py
@@ -45,69 +45,69 @@ class GBCModel(object):
         print("GBC model send-feedback entered")
         print(f"Truth: {truth}, Reward: {reward}")
 
-        if reward == 1:
-            if truth == 1:
-                self.cm["tp"] += 1
-            if truth == 0:
-                self.cm["tn"] += 1
-        if reward == 0:
-            if truth == 1:
-                self.cm["fn"] += 1
-            if truth == 0:
-                self.cm["fp"] += 1
+#         if reward == 1:
+#             if truth == 1:
+#                 self.cm["tp"] += 1
+#             if truth == 0:
+#                 self.cm["tn"] += 1
+#         if reward == 0:
+#             if truth == 1:
+#                 self.cm["fn"] += 1
+#             if truth == 0:
+#                 self.cm["fp"] += 1
 
-        self.tries += 1
-        self.success = self.success + 1 if reward else self.success
-        self.value = self.success / self.tries
+#         self.tries += 1
+#         self.success = self.success + 1 if reward else self.success
+#         self.value = self.success / self.tries
 
-        print(self.cm)
-        print(
-            "Tries: %s, successes: %s, values: %s", self.tries, self.success, self.value
-        )
+#         print(self.cm)
+#         print(
+#             "Tries: %s, successes: %s, values: %s", self.tries, self.success, self.value
+#         )
 
-    def metrics(self):
-        tp = {
-            "type": "GAUGE",
-            "key": "true_pos_total",
-            "value": self.cm["tp"],
-            "tags": {"branch_name": "GBC"},
-        }
-        tn = {
-            "type": "GAUGE",
-            "key": "true_neg_total",
-            "value": self.cm["tn"],
-            "tags": {"branch_name": "GBC"},
-        }
-        fp = {
-            "type": "GAUGE",
-            "key": "false_pos_total",
-            "value": self.cm["fp"],
-            "tags": {"branch_name": "GBC"},
-        }
-        fn = {
-            "type": "GAUGE",
-            "key": "false_neg_total",
-            "value": self.cm["fn"],
-            "tags": {"branch_name": "GBC"},
-        }
+#     def metrics(self):
+#         tp = {
+#             "type": "GAUGE",
+#             "key": "true_pos_total",
+#             "value": self.cm["tp"],
+#             "tags": {"branch_name": "GBC"},
+#         }
+#         tn = {
+#             "type": "GAUGE",
+#             "key": "true_neg_total",
+#             "value": self.cm["tn"],
+#             "tags": {"branch_name": "GBC"},
+#         }
+#         fp = {
+#             "type": "GAUGE",
+#             "key": "false_pos_total",
+#             "value": self.cm["fp"],
+#             "tags": {"branch_name": "GBC"},
+#         }
+#         fn = {
+#             "type": "GAUGE",
+#             "key": "false_neg_total",
+#             "value": self.cm["fn"],
+#             "tags": {"branch_name": "GBC"},
+#         }
 
-        value = {
-            "type": "GAUGE",
-            "key": "branch_value",
-            "value": self.value,
-            "tags": {"branch_name": "GBC"},
-        }
-        success = {
-            "type": "GAUGE",
-            "key": "n_success_total",
-            "value": self.success,
-            "tags": {"branch_name": "GBC"},
-        }
-        tries = {
-            "type": "GAUGE",
-            "key": "n_tries_total",
-            "value": self.tries,
-            "tags": {"branch_name": "GBC"},
-        }
+#         value = {
+#             "type": "GAUGE",
+#             "key": "branch_value",
+#             "value": self.value,
+#             "tags": {"branch_name": "GBC"},
+#         }
+#         success = {
+#             "type": "GAUGE",
+#             "key": "n_success_total",
+#             "value": self.success,
+#             "tags": {"branch_name": "GBC"},
+#         }
+#         tries = {
+#             "type": "GAUGE",
+#             "key": "n_tries_total",
+#             "value": self.tries,
+#             "tags": {"branch_name": "GBC"},
+#         }
 
-        return [tp, tn, fp, fn, value, success, tries]
+#         return [tp, tn, fp, fn, value, success, tries]
