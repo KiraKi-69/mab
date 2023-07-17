c68d292f2471a058dcdcdeff5df5f66b71ddaa8f
Alex Gorokhov <agorokhov@neoflex.ru>
2023-07-17 14:33:25 +0300

    feat: change deployments and test ipynb

--- DTCModel.py
+++ DTCModel.py
@@ -44,69 +44,69 @@ class DTCModel(object):
         print("DTC model send-feedback entered")
         print(f"Truth: {truth}, Reward: {reward}")
 
-#         if reward == 1:
-#             if truth == 1:
-#                 self.cm["tp"] += 1
-#             if truth == 0:
-#                 self.cm["tn"] += 1
-#         if reward == 0:
-#             if truth == 1:
-#                 self.cm["fn"] += 1
-#             if truth == 0:
-#                 self.cm["fp"] += 1
+        if reward == 1:
+            if truth == 1:
+                self.cm["tp"] += 1
+            if truth == 0:
+                self.cm["tn"] += 1
+        if reward == 0:
+            if truth == 1:
+                self.cm["fn"] += 1
+            if truth == 0:
+                self.cm["fp"] += 1
 
-#         self.tries += 1
-#         self.success = self.success + 1 if reward else self.success
-#         self.value = self.success / self.tries
+        self.tries += 1
+        self.success = self.success + 1 if reward else self.success
+        self.value = self.success / self.tries
 
-#         print(self.cm)
-#         print(
-#             "Tries: %s, successes: %s, values: %s", self.tries, self.success, self.value
-#         )
+        print(self.cm)
+        print(
+            "Tries: %s, successes: %s, values: %s", self.tries, self.success, self.value
+        )
 
-#     def metrics(self):
-#         tp = {
-#             "type": "GAUGE",
-#             "key": "true_pos_total",
-#             "value": self.cm["tp"],
-#             "tags": {"branch_name": "DTC"},
-#         }
-#         tn = {
-#             "type": "GAUGE",
-#             "key": "true_neg_total",
-#             "value": self.cm["tn"],
-#             "tags": {"branch_name": "DTC"},
-#         }
-#         fp = {
-#             "type": "GAUGE",
-#             "key": "false_pos_total",
-#             "value": self.cm["fp"],
-#             "tags": {"branch_name": "DTC"},
-#         }
-#         fn = {
-#             "type": "GAUGE",
-#             "key": "false_neg_total",
-#             "value": self.cm["fn"],
-#             "tags": {"branch_name": "DTC"},
-#         }
+    def metrics(self):
+        tp = {
+            "type": "GAUGE",
+            "key": "true_pos_total",
+            "value": self.cm["tp"],
+            "tags": {"branch_name": "DTC"},
+        }
+        tn = {
+            "type": "GAUGE",
+            "key": "true_neg_total",
+            "value": self.cm["tn"],
+            "tags": {"branch_name": "DTC"},
+        }
+        fp = {
+            "type": "GAUGE",
+            "key": "false_pos_total",
+            "value": self.cm["fp"],
+            "tags": {"branch_name": "DTC"},
+        }
+        fn = {
+            "type": "GAUGE",
+            "key": "false_neg_total",
+            "value": self.cm["fn"],
+            "tags": {"branch_name": "DTC"},
+        }
 
-#         value = {
-#             "type": "GAUGE",
-#             "key": "branch_value",
-#             "value": self.value,
-#             "tags": {"branch_name": "DTC"},
-#         }
-#         success = {
-#             "type": "GAUGE",
-#             "key": "n_success_total",
-#             "value": self.success,
-#             "tags": {"branch_name": "DTC"},
-#         }
-#         tries = {
-#             "type": "GAUGE",
-#             "key": "n_tries_total",
-#             "value": self.tries,
-#             "tags": {"branch_name": "DTC"},
-#         }
+        value = {
+            "type": "GAUGE",
+            "key": "branch_value",
+            "value": self.value,
+            "tags": {"branch_name": "DTC"},
+        }
+        success = {
+            "type": "GAUGE",
+            "key": "n_success_total",
+            "value": self.success,
+            "tags": {"branch_name": "DTC"},
+        }
+        tries = {
+            "type": "GAUGE",
+            "key": "n_tries_total",
+            "value": self.tries,
+            "tags": {"branch_name": "DTC"},
+        }
 
-#         return [tp, tn, fp, fn, value, success, tries]
+        return [tp, tn, fp, fn, value, success, tries]
